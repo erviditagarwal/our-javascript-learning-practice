830998a866e15f75e701f6461f4ba56ec5d29582
Vidit Agarwal <56408056+erviditagarwal@users.noreply.github.com>
2023-08-26 16:44:18 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('VIDIT')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
