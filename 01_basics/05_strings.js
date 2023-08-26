b2f6bad69e66f76aca16c4a6da71134e61cd2f84
Vidit Agarwal <56408056+erviditagarwal@users.noreply.github.com>
2023-08-26 16:47:53 +0000

    c

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,7 +26,7 @@ const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://google.com"
 
 console.log(url.replace('%20', '-'))
 
