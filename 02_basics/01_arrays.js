6e723e580df6d364c4984d09bbab7c5faa942a3c
Vidit Agarwal <56408056+erviditagarwal@users.noreply.github.com>
2023-08-27 18:37:08 +0000

    Addon

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,23 +8,23 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+// myArr.push(6)  // Add Values
+// myArr.push(7)   
+// myArr.pop()    // Remove Last Value
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9)   
+// myArr.shift()   
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join()  // Join Converted values into String
 
 // console.log(myArr);
 // console.log( newArr);
 
 
-// slice, splice
+// slice, splice  
 
 console.log("A ", myArr);
 
