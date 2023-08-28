313ebba16b5a4d0a882283b126e0b730af7de526
Vidit Agarwal <56408056+erviditagarwal@users.noreply.github.com>
2023-08-28 15:21:20 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+  // singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Vidit",
+    "full name": "Vidit Agarwal",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "user@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "user@google.com"
+// Object.freeze(JsUser)
+JsUser.email = "user@google.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
